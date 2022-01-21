6ac5799e6089b787f1e96f30fb88b46b0fbbd86b
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-01-21 11:17:19 -0300

    subrotas dentro de rotas

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,20 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
     fun helloWorld(): String {
         return "Fala meu parceiro!"
+    }
+
+    @GetMapping("2")
+    fun customer2(): String {
+        return "Fala meu parceiro, esta é a rota 2, dentro de /custumer"
     }
 }
