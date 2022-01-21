dd2af88ebd634c987e30a82f35954454a593a1dc
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-01-21 18:55:10 -0300

    adiciona delete customers by id

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -20,18 +20,37 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id: Int): CustomerModel? {
         for (customer in customers) {
-            if(customer.id == id) {
+            if (customer.id == id) {
                 return customer
             }
         }
         return null
     }
 
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Int, @RequestBody customer: PostCustomerRequest): CustomerModel? {
+//        for (index in customers) {
+//            if (index.id == id) {
+//                index.name = customer.name
+//                index.email = customer.email
+//                return CustomerModel(id, customer.name, customer.email)
+//            }
+//        }
+//        return null
+
+        customers.filter { it.id == id }.first().let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+
+        return CustomerModel(id, customer.name, customer.email)
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest): PostCustomerRequest {
 
-        var id: Int = if(customers.isEmpty()) {
+        var id: Int = if (customers.isEmpty()) {
             1
         } else {
             customers.last().id + 1
@@ -40,4 +59,11 @@ class CustomerController {
         customers.add(CustomerModel(id, customer.name, customer.email))
         return customer
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        customers.removeIf { it.id == id }
+    }
+
 }
