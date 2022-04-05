18d5439851af554f011c31cb2f8ec973baa9c9e4
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-04-05 09:29:18 -0300

    add fun deleteByCustomer

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -3,12 +3,14 @@ package com.mercadolivro.service
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.BookRepository
 import com.mercadolivro.repository.CustomerRepository
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService(
-    val customerRepository: CustomerRepository
+    val customerRepository: CustomerRepository,
+    val bookService: BookService
 ) {
 
     fun getAll(name: String?): List<CustomerModel> {
@@ -41,7 +43,10 @@ class CustomerService(
 
     fun delete(id: Int) {
         if (customerRepository.existsById(id)) {
-            return customerRepository.deleteById(id)
+            kotlin.runCatching {
+                getCustomerById(id).apply { bookService.deleteByCustomer(this) }
+                return customerRepository.deleteById(id)
+            }
         }
 
         throw Exception()
